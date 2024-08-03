355f5a7c590e8b52e18cdbfc622cc20a948d348a
Risav Karna <git@risav.dev>
2024-08-03 18:01:30 +0545

    feat(ui): final link fix before moving to mothership

--- packages/astronomy-ui/src/components/nav/zen.ts
+++ packages/astronomy-ui/src/components/nav/zen.ts
@@ -1,67 +1,67 @@
 export const sidebarNav = [
   {
     title: "User Accounts",
-    href: "/dashboard/user-list",
+    href: "/cardman/user-list",
     items: [
       {
         title: "Users",
-        href: "/dashboard/user-list",
+        href: "/cardman/user-list",
         items: [],
       },
       {
         title: "User Profiles",
-        href: "/dashboard/user-profile-list",
+        href: "/cardman/user-profile-list",
         items: [],
       },
       {
         title: "User Groups",
-        href: "/dashboard/user-group-list",
+        href: "/cardman/user-group-list",
         items: [],
       },
       {
         title: "User Assets",
-        href: "/dashboard/user-asset-list",
+        href: "/cardman/user-asset-list",
         items: [],
       }
     ],
   },
   {
     title: "Cards & Scores",
-    href: "/dashboard/card-list",
+    href: "/cardman/card-list",
     items: [
       {
         title: "Cards",
-        href: "/dashboard/card-list",
+        href: "/cardman/card-list",
         items: [],
       },
       {
         title: "Credit Scores",
-        href: "/dashboard/card-score-list",
+        href: "/cardman/card-score-list",
         items: [],
       },
       {
         title: "Prepaid Limits",
-        href: "/dashboard/card-limit-list",
+        href: "/cardman/card-limit-list",
         items: [],
       },
       {
         title: "Issuance Request",
-        href: "/dashboard/card-issuance-request-list",
+        href: "/cardman/card-issuance-request-list",
         items: [],
       },
       {
         title: "Application Review",
-        href: "/dashboard/card-application-review-list",
+        href: "/cardman/card-application-review-list",
         items: [],
       },
       {
         title: "Update Request",
-        href: "/dashboard/card-update-request-list",
+        href: "/cardman/card-update-request-list",
         items: [],
       },
       {
         title: "Closing Request",
-        href: "/dashboard/card-closing-request-list",
+        href: "/cardman/card-closing-request-list",
         items: [],
       }
     ],
